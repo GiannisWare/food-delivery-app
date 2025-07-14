87673591d35c9cc1fcd85cb8c0caff0a31ff7eb3
GiannisWare <johngio20@gmail.com>
2025-07-14 11:52:04 +0300

    Crated home screen, added tailwind styles

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,60 @@
 import "./globals.css";
-import {Text, View} from "react-native";
-import {Stack} from "expo-router";
+import {FlatList, Image, Pressable, SafeAreaView, Text, TouchableOpacity, View} from "react-native";
+import cn from "clsx";
+import {images, offers} from "@/constants";
+import {Fragment} from "react";
+import CartButton from "@/components/CartButton";
 
 export default function Index() {
     return (
-        <View className=" flex-1 justify-center items-center">
-            <Text className="text-5xl text-center font-quicksand-bold text-primary">
-                Welcome to Nativewind!
-            </Text>
-        </View>
+        <SafeAreaView className="flex-1 bg-white">
+
+            <FlatList
+                data={offers}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({item, index}) => {
+                    const isEven = index % 2 === 0;
+
+                    return (
+                        <View className="p-4">
+                            <Pressable
+                                className={cn("offer-card", isEven ? "flex-row-reverse" : "flex-row")}
+                                style={{backgroundColor: item.color}}
+                                android_ripple={{color: '#ffffff22'}}
+                            >
+                                {({pressed}) => (
+                                    <Fragment>
+                                        <View className="h-full w-1/2">
+                                            <Image source={item.image} className="size-full" resizeMode="contain"/>
+                                        </View>
+
+                                        <View className={cn('offer-card__info', isEven ? 'pl-10' : 'pr-10')}>
+                                            <Text className="h1-bold text-white leading-tight">{item.title}</Text>
+                                            <Image source={images.arrowRight} className='size-10' resizeMode='contain'
+                                                   tintColor='#ffffff'/>
+                                        </View>
+                                    </Fragment>
+                                )}
+                            </Pressable>
+                        </View>
+                    );
+                }}
+                contentContainerClassName='pb-28 px-5'
+                ListHeaderComponent={({}) => (
+
+                    <View className="flex-between flex-row w-full my-5">
+                        <View className="flex-start">
+                            <Text className="small-bold text-primary">DELIVER TO</Text>
+
+                            <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
+                                <Image source={images.arrowDown} className='size-3' resizeMode="contain"/>
+                                <Text className="paragraph-bold text-dark-100 ">Greece</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <CartButton/>
+                    </View>
+                )}
+            />
+        </SafeAreaView>
     );
 }
