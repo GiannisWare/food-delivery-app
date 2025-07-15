655b70412d8b34d526a69a383203873456eb5442
GiannisWare <johngio20@gmail.com>
2025-07-16 00:08:51 +0300

    Implemented tabs, prettier setup

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,20 +1,27 @@
 import "../globals.css";
-import {Button, FlatList, Image, Pressable, SafeAreaView, Text, TouchableOpacity, View} from "react-native";
+import {
+    Button,
+    FlatList,
+    Image,
+    Pressable,
+    SafeAreaView,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native";
 import cn from "clsx";
 import {images, offers} from "@/constants";
 import {Fragment} from "react";
-import CartButton from "@/components/CartButton"
+import CartButton from "@/components/CartButton";
 import useAuthStore from "@/store/auth.store";
-
 
 export default function Index() {
     const {user} = useAuthStore();
 
-    console.log('User', JSON.stringify(user, null, 2));
+    console.log("User", JSON.stringify(user, null, 2));
 
     return (
         <SafeAreaView className="flex-1 bg-white">
-
             <FlatList
                 data={offers}
                 keyExtractor={(item, index) => index.toString()}
@@ -24,20 +31,38 @@ export default function Index() {
                     return (
                         <View className="p-4">
                             <Pressable
-                                className={cn("offer-card", isEven ? "flex-row-reverse" : "flex-row")}
+                                className={cn(
+                                    "offer-card",
+                                    isEven ? "flex-row-reverse" : "flex-row",
+                                )}
                                 style={{backgroundColor: item.color}}
-                                android_ripple={{color: '#ffffff22'}}
+                                android_ripple={{color: "#ffffff22"}}
                             >
                                 {({pressed}) => (
                                     <Fragment>
                                         <View className="h-full w-1/2">
-                                            <Image source={item.image} className="size-full" resizeMode="contain"/>
+                                            <Image
+                                                source={item.image}
+                                                className="size-full"
+                                                resizeMode="contain"
+                                            />
                                         </View>
 
-                                        <View className={cn('offer-card__info', isEven ? 'pl-10' : 'pr-10')}>
-                                            <Text className="h1-bold text-white leading-tight">{item.title}</Text>
-                                            <Image source={images.arrowRight} className='size-10' resizeMode='contain'
-                                                   tintColor='#ffffff'/>
+                                        <View
+                                            className={cn(
+                                                "offer-card__info",
+                                                isEven ? "pl-10" : "pr-10",
+                                            )}
+                                        >
+                                            <Text className="h1-bold text-white leading-tight">
+                                                {item.title}
+                                            </Text>
+                                            <Image
+                                                source={images.arrowRight}
+                                                className="size-10"
+                                                resizeMode="contain"
+                                                tintColor="#ffffff"
+                                            />
                                         </View>
                                     </Fragment>
                                 )}
@@ -45,23 +70,24 @@ export default function Index() {
                         </View>
                     );
                 }}
-                contentContainerClassName='pb-28 px-5'
+                contentContainerClassName="pb-28 px-5"
                 ListHeaderComponent={({}) => (
-
                     <View className="flex-between flex-row w-full my-5">
                         <View className="flex-start">
                             <Text className="small-bold text-primary">DELIVER TO</Text>
 
                             <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
-                                <Image source={images.arrowDown} className='size-3' resizeMode="contain"/>
+                                <Image
+                                    source={images.arrowDown}
+                                    className="size-3"
+                                    resizeMode="contain"
+                                />
                                 <Text className="paragraph-bold text-dark-100 ">Greece</Text>
                             </TouchableOpacity>
                         </View>
                         <CartButton/>
                     </View>
                 )}
-
-
             />
         </SafeAreaView>
     );
